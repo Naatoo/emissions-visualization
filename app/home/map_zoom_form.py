eee1f21a3c118e51e7960686d6934f67e31baf04
Naatoo <thenatooorat@gmail.com>
2019-06-01 17:40:25 +0200

    Enable to use 1x1degree data file

--- app/home/map_zoom_form.py
+++ app/home/map_zoom_form.py
@@ -8,12 +8,13 @@ class MapForm(FlaskForm):
     """
     Form for users to add new energy bill
     """
-    lat_min = FloatField('Latitude Start', validators=[DataRequired()], default=19.55)
-    lat_max = FloatField('Latitude End', validators=[DataRequired()], default=20.55)
-    lon_min = FloatField('Longitude Start', validators=[DataRequired()], default=49.55)
-    lon_max = FloatField('Longitude End', validators=[DataRequired()], default=50.55)
+    lon_min = FloatField('Longitude Start', validators=[DataRequired()], default=10)
+    lon_max = FloatField('Longitude End', validators=[DataRequired()], default=20)
+    lat_min = FloatField('Latitude Start', validators=[DataRequired()], default=40.9)
+    lat_max = FloatField('Latitude End', validators=[DataRequired()], default=50.9)
     interpolation = SelectField('Interpolation', validators=[DataRequired()],
-                                choices=[("1", "No interpolation"), ("2", "x2"), ("3", "x3"), ("5", "x5"), ("10", "x10"), ("25", "x25")], default=2)
+                                choices=[("1", "No interpolation"), ("2", "x2"), ("3", "x3"),
+                                         ("5", "x5"), ("10", "x10"), ("25", "x25")], default=2)
     line_opacity = FloatField('Line opacity', validators=[DataRequired()], default=0)
     fill_opacity = FloatField('Fill opacity', validators=[DataRequired()], default=0.7)
     color = SelectField('Colors', choices=[(color, color) for color in [
