5c4d17b62a7ea8ab45a05ac4437529ec9ba1c5ca
Naatoo <thenatooorat@gmail.com>
2019-06-03 00:00:57 +0200

    Rename 'data' to 'data_center'

--- app/tools/paths.py
+++ app/tools/paths.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 MAIN_DIR = str(Path(__file__).parent.parent)
-DATA_DIR = os.path.join(MAIN_DIR, "data/")
+DATA_DIR = os.path.join(MAIN_DIR, "data_center/")
 RAW_DATA_DIR = os.path.join(DATA_DIR, "raw/")
 OUTPUT_DATA_DIR = os.path.join(DATA_DIR, "output/")
 PM10_RAW_FILE = os.path.join(RAW_DATA_DIR, "PM10_raw.txt")
