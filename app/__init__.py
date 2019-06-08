ba449b52e0781bae1406722114dfbb8a0c627f16
Naatoo <thenatooorat@gmail.com>
2019-06-08 15:03:33 +0200

    Enable initial data insertion

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,82 @@
-from flask import Flask, render_template, send_file, redirect, url_for
+from flask import Flask
 
 from flask_bootstrap import Bootstrap
-from numpy import mean
 import os
 from .home import home as home_blueprint
 from .data_center.selection import data_center as data_center_blueprint
 from app.home.views import generate_map_by_coordinates, generate_map_by_country
+from app.database.database import db
 
+from .data_center.loader.excel_parser import ExcelFileParser
+from .data_center.loader.csv_parser import CSVFileParser
+from app.tools.paths import EUROPE_EMISSION_PM10_2015_EXCEL_FILE, COUNTRIES_CENTROIDS_CSV, PM10_RAW_FILE
+from app.models.emission_data import DataValues, DataInfo
+from app.tools.paths import DATABASE_FILE
+
+import secrets
 
 
 def create_app():
-    generate_map_by_country()
     app = Flask(__name__)
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_FILE}"
     Bootstrap(app)
+    db.init_app(app)
     app.register_blueprint(home_blueprint)
     app.register_blueprint(data_center_blueprint)
     return app
 
+
+def setup_database(app):
+    with app.app_context():
+        db.create_all()
+        # insert_initial_europe_data()
+        # insert_initial_emep_data()
+
+
+def insert_initial_europe_data():
+    dataset_hash = secrets.token_hex(nbytes=4)
+    physical_quantity = "concentration"
+    year = 2015
+    name = "PM10 normal"
+    parser = ExcelFileParser(file_name=EUROPE_EMISSION_PM10_2015_EXCEL_FILE)
+
+    db.session.add(DataInfo(
+        dataset_hash=dataset_hash,
+        physical_quantity=physical_quantity,
+        year=year,
+        name=name
+    ))
+    db.session.commit()
+    for (lon, lat, value) in parser.parse_data_file():
+        db.session.add(DataValues(dataset_hash=dataset_hash,
+                                  lon=lon,
+                                  lat=lat,
+                                  value=value
+                                  ))
+        db.session.flush()
+    db.session.commit()
+
+
+def insert_initial_emep_data():
+    dataset_hash = secrets.token_hex(nbytes=4)
+    physical_quantity = "emission"
+    year = 2015
+    name = "PM10 yearly"
+    parser = CSVFileParser(file_name=PM10_RAW_FILE)
+
+    db.session.add(DataInfo(
+        dataset_hash=dataset_hash,
+        physical_quantity=physical_quantity,
+        year=year,
+        name=name
+    ))
+    for (lon, lat, value) in parser.parse_data_file():
+        db.session.add(DataValues(dataset_hash=dataset_hash,
+                                  lon=lon,
+                                  lat=lat,
+                                  value=value
+                                  ))
+        # db.session.flush()
+    db.session.commit()
