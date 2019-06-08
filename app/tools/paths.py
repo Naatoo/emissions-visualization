8efc17fa93a0bdaa35dc9ec6a0b0467e0d24e31d
Naatoo <thenatooorat@gmail.com>
2019-06-08 15:02:12 +0200

    Add sqlite3 database

--- app/tools/paths.py
+++ app/tools/paths.py
@@ -3,13 +3,20 @@ from pathlib import Path
 
 
 MAIN_DIR = str(Path(__file__).parent.parent)
+
 DATA_DIR = os.path.join(MAIN_DIR, "data_center/")
 RAW_DATA_DIR = os.path.join(DATA_DIR, "raw/")
 OUTPUT_DATA_DIR = os.path.join(DATA_DIR, "output/")
 PM10_RAW_FILE = os.path.join(RAW_DATA_DIR, "PM10_raw.txt")
 PM10_JSON_FILE = os.path.join(OUTPUT_DATA_DIR, "PM10_zoomed.json")
 PM10_CSV_FILE = os.path.join(OUTPUT_DATA_DIR, "PM10_zoomed.csv")
-EMISSION_EXCEL_FILE = os.path.join(RAW_DATA_DIR, 'emission.xlsx')
+
+EUROPE_EMISSION_PM25_2015_EXCEL_FILE = os.path.join(RAW_DATA_DIR, 'europe_PM25_2015.xlsx')
+EUROPE_EMISSION_PM10_2015_EXCEL_FILE = os.path.join(RAW_DATA_DIR, 'europe_PM10_2015.xlsx')
+
 COUNTRIES_TXT_FILE = os.path.join(RAW_DATA_DIR, 'countries.txt')
 BOUNDING_BOXES_JSON = os.path.join(RAW_DATA_DIR, 'bounding_boxes.json')
 COUNTRIES_CENTROIDS_CSV = os.path.join(RAW_DATA_DIR, "countries_centroids.csv")
+
+DATABASE_DIR = os.path.join(MAIN_DIR, "database/")
+DATABASE_FILE = os.path.join(DATABASE_DIR, "data.db")
