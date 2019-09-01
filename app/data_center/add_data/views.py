5bd098fec5ae047923f15648dc731dfccb561971
Naatoo <thenatooorat@gmail.com>
2019-09-01 04:23:45 +0200

    Store uploaded file with original name. Flash exception of longitude and latitude resolution are not numbers

--- app/data_center/add_data/views.py
+++ app/data_center/add_data/views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required
 
@@ -6,17 +8,19 @@ from app.data_center.loader.excel_parser import ExcelFileParser
 from app.data_center.add_data import data_center_add_data
 from app.data_center.add_data.forms import AddDataForm
 from app.database.queries import insert_new_file_data, get_selected_data_str
-from app.tools.paths import UPLOADED_FILE
+from app.tools.paths import UPLOADED_FILE_DIR
 
 
-@data_center_add_data.route('/data_center/add_data/<string:file_type>', methods=['GET', 'POST'])
+@data_center_add_data.route('/data_center/add_data', methods=['GET', 'POST'])
 @login_required
-def add_data(file_type):
+def add_data():
+    filename = next(file for file in os.listdir(UPLOADED_FILE_DIR))
     parsers_mapping = {
         "xlsx": ExcelFileParser,
-        "emep": EmepTxtFileParser
+        "txt": EmepTxtFileParser
     }
-    parser = parsers_mapping[file_type](UPLOADED_FILE)
+    file_type = filename.split(".")[1]
+    parser = parsers_mapping[file_type](f'{UPLOADED_FILE_DIR}/{filename}')
     preview_data = parser.get_data_for_preview(limit=20)
     form = AddDataForm()
     if form.is_submitted():
@@ -38,8 +42,14 @@ def add_data(file_type):
             "lat_resolution": lat_resolution,
             "relative_data": relative_data
         }
+        try:
+            float(lon_resolution)
+            float(lat_resolution)
+        except ValueError:
+            flash("Longitude and latitude resolution must be numbers.", category='warning')
+            return redirect(url_for("data_center_upload_file.upload_file"))
         insert_new_file_data(parser, **metadata)
-        flash(f"ADDED: {name} of {physical_quantity} from year {year}.", category='success')
+        flash(f"Dataset {name}, {compound}, {physical_quantity}, {year} was added.", category='success')
         return redirect(url_for("data_center_upload_file.upload_file"))
     selected_data_str = get_selected_data_str()
     return render_template("data_center_add_data.html", form=form,
--- app/data_center/upload_file/views.py
+++ app/data_center/upload_file/views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required
 from werkzeug.utils import secure_filename
@@ -7,7 +9,7 @@ from app.data_center.loader.excel_parser import ExcelFileParser
 from app.data_center.upload_file import data_center_upload_file
 from app.data_center.upload_file.forms import UploadFileForm
 from app.database.queries import get_selected_data_str
-from app.tools.paths import UPLOADED_FILE
+from app.tools.paths import UPLOADED_FILE_DIR
 
 
 @data_center_upload_file.route('/data_center/upload_file', methods=['GET', 'POST'])
@@ -15,10 +17,11 @@ from app.tools.paths import UPLOADED_FILE
 def upload_file():
     form = UploadFileForm()
     if form.is_submitted():
+        remove_previously_uploaded_file()
         filename = secure_filename(form.file.data.filename)
-        possible_extensions = {"xlsx": "xlsx", "emep": "txt"}
-        form.file.data.save(UPLOADED_FILE)
         file_type = form.file_type.data
+        form.file.data.save(f'{UPLOADED_FILE_DIR}/{filename}')
+        possible_extensions = {"xlsx": "xlsx", "emep": "txt"}
         error_msg = None
         if not filename.endswith(possible_extensions[file_type]):
             error_msg = f"{filename} has other extension than expected for this file type: {file_type}."
@@ -26,9 +29,11 @@ def upload_file():
             error_msg = f"{filename} has non-unique coordinates."
         if error_msg is not None:
             flash(error_msg, category="danger")
+            remove_previously_uploaded_file()
             return render_template("data_center_upload_file.html", form=form, selected_data_str=get_selected_data_str())
         else:
-            return redirect(url_for("data_center_add_data.add_data", file_type=file_type))
+            flash(f"Add metadata for dataset: {filename}", category='info')
+            return redirect(url_for("data_center_add_data.add_data"))
 
     return render_template("data_center_upload_file.html", form=form, selected_data_str=get_selected_data_str())
 
@@ -37,8 +42,14 @@ def validate_file_coordinates_unique(filename: str) -> bool:
     extension = filename.split(".")[1]
     parsers_mapping = {
         "xlsx": ExcelFileParser,
-        "emep": EmepTxtFileParser
+        "txt": EmepTxtFileParser
     }
-    rows_len = len(list(parsers_mapping[extension](UPLOADED_FILE).rows_generator()))
-    unique_rows_len = len(list(set(tup[:2] for tup in parsers_mapping[extension](UPLOADED_FILE).rows_generator())))
+    file_path = next(f'{UPLOADED_FILE_DIR}/{file}' for file in os.listdir(UPLOADED_FILE_DIR))
+    rows_len = len(list(parsers_mapping[extension](file_path).rows_generator()))
+    unique_rows_len = len(list(set(tup[:2] for tup in parsers_mapping[extension](file_path).rows_generator())))
     return rows_len == unique_rows_len
+
+
+def remove_previously_uploaded_file():
+    for name in os.listdir(UPLOADED_FILE_DIR):
+        os.remove(f'{UPLOADED_FILE_DIR}/{name}')
